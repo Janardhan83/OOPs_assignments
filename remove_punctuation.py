013977177800284be10efad0557704cdebe6442e
JANARDHAN MUNTIMADUGU <janardhanm2003@gmail.com>
2026-05-20 15:15:24 +0530

    Create remove_punctuation.py

---
+++ remove_punctuation.py
@@ -0,0 +1,30 @@
+import sys
+import string
+class PUNCTUATION:
+  def __init__(self , a):
+    try:
+      self.a=a
+    except Exception as e:
+      er_t,er_m,er_l = sys.exc_info()
+      print(f"Error in Line no : {er_l.tb_lineno} : Reason  : {er_m} :due to {er_t}")
+  def remove_punctuations(self):
+    try:
+      c=''
+      for i in self.a:
+        if i not in string.punctuation: # !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ 
+          c=c+i
+      return c
+    except Exception as e:
+      er_t,er_m,er_l = sys.exc_info()
+      print(f"Error in Line no : {er_l.tb_lineno} : Reason  : {er_m} :due to {er_t}")
+try:
+  aa=input('enter a text:')
+  texts =PUNCTUATION(aa)
+except Exception as e:
+      er_t,er_m,er_l = sys.exc_info()
+      print(f"Error in Line no : {er_l.tb_lineno} : Reason  : {er_m} :due to {er_t}")
+
+print(texts.remove_punctuations())
+"""
+enter a text: data science *(&)*&^ and a*&&^i
+ data science  and ai """
